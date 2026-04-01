8f1c0683e12197603d31f8187fc42d6a36829420
kunaljaiswal0977 <kunaljaiswal0977@gmail.com>
2026-03-31 22:14:52 -1100

    first c

---
+++ size.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+     printf("size of int data type is :%d", sizeof(int))
+      printf("size of char data type is :%d", sizeof(int))
+       printf("size of float data type is :%d", sizeof(int))
+        printf("size of data data type is :%d", sizeof(int))
+return 0;
+}
